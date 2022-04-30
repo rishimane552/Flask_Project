067b65e707f808cd6c17d7dcffb02bec8987b8f5
rishimane552 <82177460+rishimane552@users.noreply.github.com>
2022-04-30 04:26:56 -0400

    test changes

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,6 @@ from app.error_handlers import error_handlers
 from app.logging_config import log_con, LOGGING_CONFIG
 from app.simple_pages import simple_pages
 from app.songs import songs
-from app.map import map
 from app.db import database
 from flask_cors import CORS
 login_manager = flask_login.LoginManager()
