f86eab5dd1685f32c10f3a538d06009d97d71b5c
rishimane552 <82177460+rishimane552@users.noreply.github.com>
2022-05-12 00:16:43 -0400

    conftest file

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,7 +30,7 @@ def application():
 def add_user(application):
     with application.app_context():
         #new record
-        user = User('test@test.com', 'test1234')
+        user = User('test@gmail.com', 'test1234')
         db.session.add(user)
         db.session.commit()
 
